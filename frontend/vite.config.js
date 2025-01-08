7342c77d56d71deea982b0db2081f5ae137d651c
ilyas-hub <91405460+ilyas-hub@users.noreply.github.com>
2025-01-08 14:50:34 +0530

    port set

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 10000,      // or any port Render expects
+    host: '0.0.0.0',  // Ensures app is accessible from the network
+  },
+});
